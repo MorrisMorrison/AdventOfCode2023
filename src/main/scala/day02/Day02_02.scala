5d8aa4384b54b205af3c37afbf3035c570246da7
Robert Mang <Robert.Mang@softgarden.de>
2023-12-02 12:18:12 +0100

    solve day02_02

--- src/main/scala/day02/Day02_02.scala
+++ src/main/scala/day02/Day02_02.scala
@@ -1,29 +1,24 @@
 package day02
 
 import utils.FileReader
-
+import scala.collection.mutable.Map
 class Day02_02 {
   def solve(): String = {
-    val cubesConfiguration: Map[String, Int] =
-      Map("red" -> 12, "green" -> 13, "blue" -> 14)
     val lines: List[String] =
       FileReader().readLinesAsList("src/main/scala/day02/input.txt")
     var result = 0
     for (i <- 0 until lines.length) do
+      val cubesConfiguration: Map[String, Int] = Map("red" -> 0, "green" -> 0, "blue" -> 0)
       val input = lines(i);
-      var isGameValid = true
       val draws = input
         .slice(input.indexOf(":") + 2, input.length())
         .split(";")
       for (draw <- draws) do
         for (step <- draw.split(",")) do
           val input = parseInput(step)
-          if (input._2 > cubesConfiguration(input._1))
-            isGameValid = false
-
-      if (isGameValid)
-        result += i + 1
-
+          if (cubesConfiguration(input._1) < input._2)
+            cubesConfiguration(input._1) = input._2
+      result += cubesConfiguration("red") * cubesConfiguration("green") * cubesConfiguration("blue")
     result.toString()
   }
 
