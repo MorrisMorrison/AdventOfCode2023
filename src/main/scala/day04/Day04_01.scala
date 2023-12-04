fcf87febb7f83ea765c33c2d5cec4e95badfeb16
Robert Mang <Robert.Mang@softgarden.de>
2023-12-04 10:57:56 +0100

    shorten method

--- src/main/scala/day04/Day04_01.scala
+++ src/main/scala/day04/Day04_01.scala
@@ -10,15 +10,14 @@ class Day04_01 {
     val lines = FileReader().readLinesAsList("src/main/scala/day04/input.txt")
     
     lines
-      .map(s => s.slice(s.indexOf(':') + 1, s.length()))
-      .map(s => {
-        val Array(left, right) = s.split("\\|").map(_.trim)
+      .map(line => {
+        val rawCard = line.slice(line.indexOf(':') + 1, line.length())
+        val Array(left, right) = rawCard.split("\\|").map(_.trim)
         val winners = left.split("\\s+").map(_.trim().toInt).toList
         val myNumbers = right.split("\\s+").map(_.trim().toInt).toList
-        winners.intersect(myNumbers).length
+        val matching = winners.intersect(myNumbers).length
+        if matching == 0 then 0 else pow(2, matching - 1)
       })
-      .toList
-      .map(result => if result == 0 then 0 else pow(2, result - 1))
       .sum
       .toString()
   }
