de01dc310a0779266fb41e5016d074823ecbb84f
Robert Mang <Robert.Mang@softgarden.de>
2023-12-04 23:37:17 +0100

    solve day04_02

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,6 +5,8 @@ import day02.Day02_02
 import day03.Day03_01
 import day03.Day03_02
 import day04.Day04_01
+import day04.Day04_02
+
 @main def run: Unit = {
   println("Day01_01: " + Day01_01().solve());
   println("Day01_02: " + Day01_02().solve());
@@ -13,4 +15,5 @@ import day04.Day04_01
   println("Day03_01: " + Day03_01().solve());
   println("Day03_02: " + Day03_02().solve());
   println("Day04_01: " + Day04_01().solve());
+  println("Day04_02: " + Day04_02().solve());
 }
