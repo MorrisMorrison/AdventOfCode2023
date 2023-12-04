25600b5039ca20afbd3d24bbd0a62ec01e04570f
Robert Mang <Robert.Mang@softgarden.de>
2023-12-04 10:59:15 +0100

    formatting

--- src/main/scala/day04/Day04_01.scala
+++ src/main/scala/day04/Day04_01.scala
@@ -8,15 +8,26 @@ class Day04_01 {
 
   def solve(): String = {
     val lines = FileReader().readLinesAsList("src/main/scala/day04/input.txt")
-    
+
     lines
       .map(line => {
-        val rawCard = line.slice(line.indexOf(':') + 1, line.length())
-        val Array(left, right) = rawCard.split("\\|").map(_.trim)
-        val winners = left.split("\\s+").map(_.trim().toInt).toList
-        val myNumbers = right.split("\\s+").map(_.trim().toInt).toList
+        val rawCard = line
+          .slice(line.indexOf(':') + 1, line.length())
+        val Array(left, right) = rawCard
+          .split("\\|")
+          .map(_.trim)
+        val winners = left
+          .split("\\s+")
+          .map(_.trim().toInt)
+          .toList
+        val myNumbers = right
+          .split("\\s+")
+          .map(_.trim().toInt)
+          .toList
         val matching = winners.intersect(myNumbers).length
-        if matching == 0 then 0 else pow(2, matching - 1)
+        
+        if matching == 0 then 0
+        else pow(2, matching - 1)
       })
       .sum
       .toString()
